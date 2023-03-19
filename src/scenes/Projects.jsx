5d4e92a925305d9f4600725f3b41be3d6982f682
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-19 14:33:57 -0500

    Fix Bug Image Chat App

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -91,7 +91,7 @@ const Projects = () => {
             BEAUTIFUL USER INTERFACES
           </div>
           <Project
-            title="Chat AI App"
+            title="Chat App"
             description="React Fullstack, Chat App. The frontend will consist of ChatEngine, Redux Toolkit for our state management, Redux Toolkit Query for making api calls, Hero icons and React Router for Navigation. For the backend we will be using Node Js as our runtime, Express Js as our backend framework, and OpenAI for Ai integration with our chat."
             link="https://github.com/Carlosaj18/Fullstack-React-Chat-AI-App/"
           />
