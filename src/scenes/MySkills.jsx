dd3b9ba284c30248c7c67d2ea97e605e335b5ded
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-20 15:28:47 -0500

    Adjust skills description

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -73,9 +73,7 @@ const MySkills = () => {
             <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-            As a software developer, I have a strong track record of using my
-            problem-solving skills to overcome complex challenges in my work. I
-            have the ability to break down problems into smaller, more
+            I have the ability to break down problems into smaller, more
             manageable components and analyze them systematically to identify
             the root cause.
           </p>
@@ -103,11 +101,8 @@ const MySkills = () => {
             <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-            I understand that even the smallest mistake in code can cause
-            significant issues in the final product, and so I take great care to
-            ensure that every aspect of my work is accurate and precise. I take
-            the time to thoroughly analyze each component of a project to
-            identify potential errors or bugs.
+            I take the time to thoroughly analyze each component of a project to
+            identify potential errors or bugs, in order to ensure that every aspect of my work is accurate and precise.
           </p>
         </motion.div>
         {/* IMAGINATIVE */}
@@ -132,10 +127,9 @@ const MySkills = () => {
             <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-            I have developed a strong ability to adapt to new challenges and
-            changing circumstances. I am always eager to learn new technologies
-            and programming languages, and I am continually seeking out
-            opportunities to expand my knowledge and skill set. 
+          I have developed a strong ability to adapt to new challenges,
+            changing circumstances and eager to learn new technologies
+            and programming languages.  
           </p>
         </motion.div>
       </div>
